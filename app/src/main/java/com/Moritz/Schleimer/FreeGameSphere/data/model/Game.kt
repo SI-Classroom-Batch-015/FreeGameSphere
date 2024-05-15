f25dbcd3b2901f1735f28400d7d1a478eae62466
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-15 12:29:01 +0200

    Modelklasse game erstellt um die daten aus der API zu bekommen

---
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/model/Game.kt
@@ -0,0 +1,8 @@
+package com.Moritz.Schleimer.FreeGameSphere.data.model
+
+data class Game(
+    val title: String?,
+    val genre: String?,
+    val thumbnail: String?,
+    val platform: String?
+)
