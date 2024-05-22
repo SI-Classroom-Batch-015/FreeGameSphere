2a95691788a298c19836162c00bc727e9b1be5ab
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-22 14:31:19 +0200

    die toolbar hinzugefügt

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/GamesFragment.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/GamesFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.Moritz.Schleimer.FreeGameSphere.MainViewModel
+import com.Moritz.Schleimer.FreeGameSphere.R
 import com.Moritz.Schleimer.FreeGameSphere.adapter.GamesAdapter
 import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentGamesBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class GamesFragment: Fragment() {
 
@@ -27,6 +30,10 @@ class GamesFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.materialToolbar)
+        var toolBarTitle= toolbar.findViewById<TextView>(R.id.toolbar_title)
+        toolBarTitle.text = "Spiele Liste"
 
         val rvAdapter = GamesAdapter(emptyList())
         binding.rvGames.adapter = rvAdapter
