228427908267a56f3bcf3137e7046e885d2c568f
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-22 14:28:17 +0200

    meiner dataclass neue eigenschaften gegeben

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/model/Game.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/model/Game.kt
@@ -1,8 +1,15 @@
 package com.Moritz.Schleimer.FreeGameSphere.data.model
 
 data class Game(
+    val id: Int,
     val title: String?,
-    val genre: String?,
     val thumbnail: String?,
-    val platform: String?
+    val platform: String?,
+    val genre: String?,
+    val publisher: String?,
+    val developer: String?,
+    val release_date: String?,
+    val short_description: String?,
+    val game_url: String?,
+    val isLiked: Boolean = false
 )
