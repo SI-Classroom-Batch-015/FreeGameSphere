aaba05b233d6b92367c9ffcdd445e75223faf570
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-22 14:30:59 +0200

    navigation per set on click listener implementiert + jeweilige imports gemacht

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/adapter/GamesAdapter.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/adapter/GamesAdapter.kt
@@ -3,12 +3,14 @@ package com.Moritz.Schleimer.FreeGameSphere.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.Moritz.Schleimer.FreeGameSphere.data.model.Game
 import com.Moritz.Schleimer.FreeGameSphere.data.remote.BASE_URL
 import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentGamesBinding
 import com.Moritz.Schleimer.FreeGameSphere.databinding.ItemGameBinding
+import com.Moritz.Schleimer.FreeGameSphere.ui.GamesFragmentDirections
 
 class GamesAdapter(
     private var dataset: List<Game>
@@ -29,9 +31,12 @@ class GamesAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val game = dataset[position]
         holder.binding.ivGame.load(game.thumbnail)
-        holder.binding.tvTitel.text = game.title
         holder.binding.tvGenre.text = game.genre
-        holder.binding.tvPlatform.text = game.platform
+        holder.binding.tvPlattform.text = game.platform
+        holder.binding.cvGame.setOnClickListener {
+            val action = GamesFragmentDirections.actionGamesFragment2ToDetailGamesFragment(game.id)
+            it.findNavController().navigate(action)
+        }
     }
     @SuppressLint("NotifyDataSetChanged")
     fun submitListCall(list: List<Game>) {
