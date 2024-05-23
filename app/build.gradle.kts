475f5d70bc13e228432aa89e3c5f3c4802498400
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-23 16:55:28 +0200

    3 implementations gelöscht da noch nicht notwendig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    
+
+
+
 
     //Retrofit und Moshi
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
