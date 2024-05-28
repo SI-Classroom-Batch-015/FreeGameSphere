35190787ea95be91ca31eb85456e89acf5f308d0
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-28 12:31:38 +0200

    FirebaseService implementiert

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/Repository.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/Repository.kt
@@ -5,13 +5,16 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.Moritz.Schleimer.FreeGameSphere.data.model.Game
+import com.Moritz.Schleimer.FreeGameSphere.data.remote.FirebaseService
 import com.Moritz.Schleimer.FreeGameSphere.data.remote.FreeToGameAPIService
 import com.Moritz.Schleimer.FreeGameSphere.data.remote.GameApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 const val TAG = "Repository"
 class Repository(
-    private val api: GameApi
+    private val api: GameApi,
+    private val firebaseService: FirebaseService
+
 ) {
     private val _games = MutableLiveData<List<Game>>()
     val games: LiveData<List<Game>>
@@ -36,5 +39,8 @@ class Repository(
         }catch (e:Exception){
             Log.e(TAG,"Failed to get Game by ID from API $e")
         }
+    }
+    suspend fun signInUser(email: String, password: String): Boolean {
+        return firebaseService.signInWithEmailAndPassword(email, password)
     }
 }
