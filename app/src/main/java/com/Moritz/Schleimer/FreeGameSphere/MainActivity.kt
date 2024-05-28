15239f921cf697b66f65ca2805eed511746827e1
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-28 12:29:44 +0200

    toasts hinzugefügt

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/MainActivity.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.Moritz.Schleimer.FreeGameSphere
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        viewModel.error.observe(this){
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
     }
 }
