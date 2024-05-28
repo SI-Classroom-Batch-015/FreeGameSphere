4b7f3d9282b6380a9ddda92646b93390d4b71ee0
moritzschleimer <Schleimer.moritz@gmail.com>
2024-05-28 12:27:04 +0200

    bottom nav eingefügt und navigation erstmal obligatorisch hinzugefügt

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/GamesFragment.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/GamesFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.Moritz.Schleimer.FreeGameSphere.MainViewModel
 import com.Moritz.Schleimer.FreeGameSphere.R
 import com.Moritz.Schleimer.FreeGameSphere.adapter.GamesAdapter
 import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentGamesBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class GamesFragment: Fragment() {
 
@@ -35,10 +37,22 @@ class GamesFragment: Fragment() {
         var toolBarTitle= toolbar.findViewById<TextView>(R.id.toolbar_title)
         toolBarTitle.text = "Spiele Liste"
 
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.visibility = View.VISIBLE
+
         val rvAdapter = GamesAdapter(emptyList())
         binding.rvGames.adapter = rvAdapter
         viewModel.games.observe(viewLifecycleOwner) {
             rvAdapter.submitListCall(it)
+        }
+        binding.imageButton.setOnClickListener {
+            viewModel.logOut()
+        }
+        viewModel.user.observe(viewLifecycleOwner){fbUser->
+            if (fbUser == null){
+                val action= GamesFragmentDirections.actionGamesFragment2ToLoginFragment()
+                findNavController().navigate(action)
+            }
         }
     }
 }
