4ae14d8b88dd4b65c80db558b44b30df4de645f4
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:29:56 +0200

    meine modelclass einen wert gegeben der aber leer anfängt

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/model/Game.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/model/Game.kt
@@ -1,15 +1,15 @@
 package com.Moritz.Schleimer.FreeGameSphere.data.model
 
 data class Game(
-    val id: Int,
-    val title: String?,
-    val thumbnail: String?,
-    val platform: String?,
-    val genre: String?,
-    val publisher: String?,
-    val developer: String?,
-    val release_date: String?,
-    val description: String?,
-    val game_url: String?,
+    val id: Int = 0,
+    val title: String? = "",
+    val thumbnail: String? = "",
+    val platform: String? = "",
+    val genre: String? = "",
+    val publisher: String? = "",
+    val developer: String? = "",
+    val release_date: String? = "",
+    val description: String? = "",
+    val game_url: String? = "",
     val isLiked: Boolean = false
 )
