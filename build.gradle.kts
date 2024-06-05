c4d6d39bbf7cd6e120c67ae41497d1805c2002e0
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:23:35 +0200

    ?

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
+
 }
