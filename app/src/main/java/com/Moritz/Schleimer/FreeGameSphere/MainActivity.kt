55001c2424bb94e59b8c93612390272b18396c53
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:32:34 +0200

    der bottom navigation bar navigation gegeben

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/MainActivity.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/MainActivity.kt
@@ -2,26 +2,34 @@ package com.Moritz.Schleimer.FreeGameSphere
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.Moritz.Schleimer.FreeGameSphere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        viewModel.error.observe(this){
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHost.navController)
+
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.fragmentContainerView2.findNavController().navigateUp()
+            }
+        })
     }
 }
