c329f73c13715081c7bf05380a13870266425216
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:38:36 +0200

    ettings fragment neu eingerichtet

---
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/SettingsFragment.kt
@@ -0,0 +1,47 @@
+package com.Moritz.Schleimer.FreeGameSphere.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.Moritz.Schleimer.FreeGameSphere.MainViewModel
+import com.Moritz.Schleimer.FreeGameSphere.R
+import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentSettingsBinding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class SettingsFragment:Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.materialToolbar)
+        var toolBarTitle= toolbar.findViewById<TextView>(R.id.toolbar_title)
+        toolBarTitle.text = "Einstellungen"
+
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.visibility = View.VISIBLE
+
+        binding.btnLogout.setOnClickListener {
+            viewModel.signOut()
+            findNavController().popBackStack(R.id.loginFragment, true)
+            findNavController().navigate(R.id.loginFragment)
+
+        }
+    }
+}
