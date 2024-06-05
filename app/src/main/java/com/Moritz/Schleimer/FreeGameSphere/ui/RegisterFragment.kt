b17c73495848c770d2bce4bafcb14077359986e8
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:37:34 +0200

    der register funktion in firebase noch confirm passwort gegeben das 2 gebraucht werden und den error state observed

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/RegisterFragment.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/RegisterFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.Moritz.Schleimer.FreeGameSphere.MainViewModel
 import com.Moritz.Schleimer.FreeGameSphere.R
+import com.Moritz.Schleimer.FreeGameSphere.STATES
 import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentRegisterBinding
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -32,22 +34,43 @@ class RegisterFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.materialToolbar)
-        var toolBarTitle= toolbar.findViewById<TextView>(R.id.toolbar_title)
+        var toolBarTitle = toolbar.findViewById<TextView>(R.id.toolbar_title)
         toolBarTitle.text = ""
 
-        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        viewModel.error.observe(viewLifecycleOwner) {
+            it?.let {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                viewModel.clearError()
+            }
+        }
+
+        val bottomNav =
+            requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.visibility = View.GONE
 
         binding.btnRegister2.setOnClickListener {
-            val email= binding.emailEt.text.toString()
-            val passwort= binding.passwordEt.text.toString()
-            viewModel.attemptSignUp(email,passwort)
+            val email = binding.emailEt.text.toString()
+            val passwort = binding.passwordEt.text.toString()
+            val confirmPassword = binding.passwortSafeEt.text.toString()
+            viewModel.signUp(email, passwort,confirmPassword)
 
         }
-        viewModel.user.observe(viewLifecycleOwner){
-            if (it!= null){
-                val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
-                findNavController().navigate(action)
+        viewModel.currentState.observe(viewLifecycleOwner) {
+            when (it) {
+                STATES.LOADING -> {
+                }
+
+                STATES.SUCCESS -> {
+                    Toast.makeText(requireContext(), "Registered Successfully", Toast.LENGTH_SHORT)
+                        .show()
+                    findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
+                    viewModel.clearState()
+                }
+
+                STATES.ERROR -> {
+                }
+
+                else -> {}
             }
         }
     }
