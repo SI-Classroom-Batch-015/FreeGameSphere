452a3ad9ef7783daa766412bcf2ad5e7e969b4b0
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-05 13:31:43 +0200

    Die gesamte firebase Auth logic implementiert

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/LoginFragment.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/ui/LoginFragment.kt
@@ -5,16 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.Moritz.Schleimer.FreeGameSphere.MainViewModel
 import com.Moritz.Schleimer.FreeGameSphere.R
+import com.Moritz.Schleimer.FreeGameSphere.STATES
 import com.Moritz.Schleimer.FreeGameSphere.databinding.FragmentLoginBinding
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private val viewModel: MainViewModel by activityViewModels()
@@ -24,7 +26,7 @@ class LoginFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -32,29 +34,54 @@ class LoginFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.materialToolbar)
-        var toolBarTitle= toolbar.findViewById<TextView>(R.id.toolbar_title)
+        var toolBarTitle = toolbar.findViewById<TextView>(R.id.toolbar_title)
         toolBarTitle.text = ""
 
-        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNav =
+            requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.visibility = View.GONE
 
+        viewModel.error.observe(viewLifecycleOwner) {
+            it?.let {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                viewModel.clearError()
+            }
+        }
+        viewModel.currentUser.observe(viewLifecycleOwner) {
+            if (it != null) {
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToGamesFragment2())
+            }
+        }
         binding.btnRegister.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
             findNavController().navigate(action)
         }
 
-
         binding.btnLogin.setOnClickListener {
-            val email= binding.emailEt.text.toString()
-            val passwort= binding.passwordEt.text.toString()
+            val email = binding.emailEt.text.toString()
+            val passwort = binding.passwordEt.text.toString()
             viewModel.signIn(email, passwort)
         }
-        viewModel.user.observe(viewLifecycleOwner){fbUser->
-            if (fbUser != null){
-                val action = LoginFragmentDirections.actionLoginFragmentToGamesFragment2()
-                findNavController().navigate(action)
-            }
+        viewModel.currentState.observe(viewLifecycleOwner) {
+            when (it) {
+                STATES.LOADING -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnLogin.visibility = View.GONE
+                }
 
+                STATES.SUCCESS -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnLogin.visibility = View.VISIBLE
+                    viewModel.clearState()
+                }
+
+                STATES.ERROR -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnLogin.visibility = View.VISIBLE
+                }
+
+                else -> {}
+            }
         }
     }
 }
