73c5244d8588152823213d3b59ffc40bab981706
moritzschleimer <Schleimer.moritz@gmail.com>
2024-06-14 12:35:57 +0200

    hab mein .document gesettet und dann gemappet und ihn dann likedat mit einem Timestamp speichern lassen

--- app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/remote/FirestoreService.kt
+++ app/src/main/java/com/Moritz/Schleimer/FreeGameSphere/data/remote/FirestoreService.kt
@@ -2,7 +2,7 @@ package com.Moritz.Schleimer.FreeGameSphere.data.remote
 
 import com.Moritz.Schleimer.FreeGameSphere.data.model.Game
 import com.Moritz.Schleimer.FreeGameSphere.data.model.Profile
-import com.google.firebase.firestore.auth.User
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
@@ -22,15 +22,17 @@ class FirestoreService(
     }
 
     suspend fun addToFavorites(uid: String,game: Game){
-        database.collection("Profiles").document(uid).collection("Favorites").document(game.id.toString()).set(game).await()
+        database.collection("Profiles").document(uid).collection("Favorites").document(game.id.toString()).set(
+            mapOf("likedAt" to Timestamp.now())
+        ) .await()
     }
 
     suspend fun removeFromFavorites(uid: String,game: Game){
         database.collection("Profiles").document(uid).collection("Favorites").document(game.id.toString()).delete().await()
     }
 
-    suspend fun getFavorites(uid: String):List<Game>{
+    suspend fun getFavorites(uid: String):List<Int>{
         val result = database.collection("Profiles").document(uid).collection("Favorites").get().await()
-        return result.toObjects(Game::class.java)
+        return result.documents.map { it.id.toInt() }
     }
 }
